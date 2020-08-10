796e7a9f3a3456a4d98b521497c38a74205f5105
Demetrio <demetrioalmeida@gmail.com>
2020-08-10 00:38:48 -0700

    Updates

---
+++ precache-manifest.ecbebc5e6dc764f5483288e9e72f5017.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74dd5fcd0779052625c4dfc857dd50cf",
+    "url": "/Demetrio09/Homework_18_Budget_Tracker/index.html"
+  },
+  {
+    "revision": "c594a1cde43d42744623",
+    "url": "/Demetrio09/Homework_18_Budget_Tracker/static/css/main.9f0da1c7.chunk.css"
+  },
+  {
+    "revision": "e29521ad48ec737a908a",
+    "url": "/Demetrio09/Homework_18_Budget_Tracker/static/js/2.264f4394.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Demetrio09/Homework_18_Budget_Tracker/static/js/2.264f4394.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c594a1cde43d42744623",
+    "url": "/Demetrio09/Homework_18_Budget_Tracker/static/js/main.e8b124ce.chunk.js"
+  },
+  {
+    "revision": "08afce460b4e61467c93",
+    "url": "/Demetrio09/Homework_18_Budget_Tracker/static/js/runtime-main.8ceb5f57.js"
+  }
+]);
